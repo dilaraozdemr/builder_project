bce6a9b78a3fd9a8351004e87c5ad555d3439251
Dilara Özdemir <dilaraozdmr66@gmail.com>
2023-05-11 17:56:28 +0300

    update welcome screen

--- lib/src/features/authentication/screens/WelcomeScreen.dart
+++ lib/src/features/authentication/screens/WelcomeScreen.dart
@@ -33,24 +33,111 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   getWelcomeText("Binalarımız"),
                   getWelcomeText("Güvenli"),
                   getWelcomeText("Biz Güvendeyiz"),
-                  SizedBox(height: 200,),
+                  SizedBox(
+                    height: 210,
+                  ),
                   Material(
                     borderRadius: BorderRadius.circular(25),
                     color: Colors.transparent,
                     child: InkWell(
-                      onTap: (){
+                      onTap: () {
                         print("Butona bir kere basıldı");
                       },
                       child: Container(
                         width: 250,
-                        height: 60,
-                        decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 2),
-                        borderRadius: BorderRadius.circular(25),
-                        color: Colors.white38,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 2),
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Giriş Yap",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: "Playfair",
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Hesabınız yok mu ?  ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "PlayFair",
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "Kayıt Olun",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Playfair",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 2,
+                          width: 150,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Container(
+                          height: 2,
+                          width: 150,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Material(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(15),
+                      onTap: (){
+                        print("bir kere tıklanıldı");
+                      },
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white54,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Image.asset(
+                            "assets/images/google.png",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -64,7 +151,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Text(
       text,
       style:
-          TextStyle(color: Colors.white, fontFamily: "Playfair", fontSize: 30),
+          TextStyle(color: Colors.white, fontFamily: "Playfair", fontSize: 35, fontWeight: FontWeight.bold),
     );
   }
 }
